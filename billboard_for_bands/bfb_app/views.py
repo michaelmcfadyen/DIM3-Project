b5560458cffc4138a39810a4f6d1a93f85e598b1
Michael McFadyen <michael.mcfadyen@hotmail.co.uk>
2013-03-12 10:33:12 +0000

    f

--- billboard_for_bands/bfb_app/views.py
+++ billboard_for_bands/bfb_app/views.py
@@ -18,6 +18,7 @@ import datetime
 
 def review(request):
 	advert = Advert.objects.filter(status='CLOSED')[0]
+	advert.status='CHOSEN'
 	template = loader.get_template('bfb_app/review.html')
 	context = RequestContext(request,{'advert':advert}) 
 	return HttpResponse(template.render(context))
